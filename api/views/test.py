f836054d39033abd88cf18159f4ccbe2c4c03ad8
rrrorrigo <rodrigobriones54@gmail.com>
2021-10-14 13:59:38 +0000

    testingapi

---
+++ api/views/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" handles restful api actions """
+
+from flask import Flask, jsonify, abort, request
+from api.views import app_views
+from models import storage
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    users_list = []
+    for user in storage.all("User").values():
+        users_list.append(user.to_dict().get("username"))
+        return jsonify(users_list)
+    #users_list = jsonify(users_list)
+    #print(type(users_list))
+    #return jsonify({})
+    #return (users_list)
