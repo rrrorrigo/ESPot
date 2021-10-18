8b02e7c9e7e00a4cd45795a6b4c66ae442d8eb30
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-18 16:52:09 -0300

    js

--- web_flask/test.js
+++ web_flask/test.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-    setInterval(async function(){
-    const response = await fetch('http://35.243.197.246:5001/api/pots/0b9e88b6-7663-47c0-8ac4-b91954bd818e');
-    const json = await response.json();
-        hum.textContent = json;
-    }, 2000);
+    function updateAllEvents() { 
+        $.getJSON('http://35.243.197.246:5001/api/pots', function(data, status){
+            console.log(data);
+        });  
+    }
+    let display = setInterval(updateAllEvents, 2000);
 });
