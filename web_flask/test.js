0cf8b92213c2d6f4dfeb91a0f3e32c9d05e1e50a
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-18 17:24:40 -0300

    mazum

--- web_flask/test.js
+++ web_flask/test.js
@@ -1,8 +1,11 @@
-window.addEventListener('DOMContentLoaded', (e) => {
+/* window.addEventListener('DOMContentLoaded', (e) => {
     function updateAllEvents() { 
         fetch('http://35.243.197.246:5001/api/pots')
             .then(response => response.json())
             .then(data => console.log(data))
     }
     let display = setInterval(updateAllEvents, 2000);
-});
+}); */
+fetch('http://35.243.197.246:5001/api/pots')
+            .then(response => response.json())
+            .then(data => console.log(data))
