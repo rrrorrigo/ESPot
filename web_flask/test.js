deca1be245b14e31e95a71e27b5b5e11bef1f703
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-18 17:48:16 -0300

    gracias mazen <3

--- web_flask/test.js
+++ web_flask/test.js
@@ -1,11 +1,15 @@
-/* window.addEventListener('DOMContentLoaded', (e) => {
+window.addEventListener('DOMContentLoaded', (e) => {
     function updateAllEvents() { 
         fetch('http://35.243.197.246:5001/api/pots')
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                const hum = document.querySelector(".hum")
+                hum.innerHTML = "Humidity: " + data[0].hum
+                const irri = document.querySelector(".hum")
+                irri.innerHTML = "Time of last irrigation: " + data[0].irri
+                const tank = document.querySelector(".hum")
+                tank.innerHTML = "Is the tank empty: " + data[0].tank
+            })   
     }
     let display = setInterval(updateAllEvents, 2000);
-}); */
-fetch('http://35.243.197.246:5001/api/pots')
-            .then(response => response.json())
-            .then(data => console.log(data))
+});
