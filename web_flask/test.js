840dfe7195f5c595b1eeb253211877682afa3cd8
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-18 18:05:27 -0300

    ¿?

--- web_flask/test.js
+++ web_flask/test.js
@@ -1,16 +1,16 @@
 window.addEventListener('DOMContentLoaded', (e) => {
     function updateAllEvents() { 
+        const hum = document.querySelector(".hum")
+        
+        const irri = document.querySelector(".irri")
+        
+        const tank = document.querySelector(".tank")
         fetch('http://35.243.197.246:5001/api/pots')
-            .then(response => response.json())
-            .then(data => {
-                const hum = document.querySelector(".hum")
-                hum.innerText = "Humidity: " + data[0].Actual_humidity
-
-                const irri = document.querySelector(".irri")
-                irri.innerText = "Time of last irrigation: " + data[0].Last_irrigation
-                
-                const tank = document.querySelector(".tank")
-                tank.innerText = "Is the tank empty: " + data[0].Is_empty
+        .then(response => response.json())
+        .then(data => {
+            hum.innerText = "Humidity: " + data[0].Actual_humidity
+            irri.innerText = "Time of last irrigation: " + data[0].Last_irrigation
+            tank.innerText = "Is the tank empty: " + data[0].Is_empty
             })   
     }
     let display = setInterval(updateAllEvents, 2000);
