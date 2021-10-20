8509bcba7e3276b8783cb401ec729b44d32f4709
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-19 22:51:09 -0300

    main flask testing

--- web_flask/main.py
+++ web_flask/main.py
@@ -3,10 +3,12 @@
 runs flask application
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from models import *
 from models import storage
 from models.Pot import Pot
+from models.User import User
+from hashlib import md5
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -14,9 +16,26 @@ CORS(app)
 @app.route('/my_plants/<string:user_id>', strict_slashes=False)
 def home(user_id=""):
     """testing"""
-    
+    usr = storage.get(User, user_id)
     return render_template('/my_plants.html')
 
+@app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
+def index():
+    if request.method == 'GET':
+        user = storage.get(User, "1649e55c-4c4b-478b-a2f0-2f33dbba613a")
+        return render_template('login.html', user=user)
+    else:
+        username = request.form['name']
+        email = request.form['email']
+        pwd = request.form['password']
+        if md5(pwd.encode()).hexdigest() == storage.getByUsername(User, username).password:
+            return redirect(url_for('success'))
+        else:
+            pass
+
+@app.route('/success')
+def success():
+   return 'logged in successfully'
 
 """ 
 @app.route('/login', strict_slashes=False)
