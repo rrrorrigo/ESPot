c7f58c4302a85f54c3c25601947bbd43c8beccce
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-19 23:40:15 -0300

    w

--- web_flask/main.py
+++ web_flask/main.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/<string:user_id>/my_plants', strict_slashes=False)
+@app.route('/<user_id>/my_plants', strict_slashes=False)
 def home(user_id=""):
     """testing"""
     usr = storage.get(User, user_id)
