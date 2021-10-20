6b0184e7485a32ce349ad48cce179fba8c4cb292
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-19 21:49:57 -0300

    moving folders from web -> web_flask

---
+++ web_flask/static/scripts/primary.js
@@ -0,0 +1,21 @@
+function onoff() {
+let onoff = document.getElementById("onoff");
+if (compareColour("#808D80", onoff.style.color)) {
+    onoff.style.color = "rgb(71, 117, 65)";
+} else {
+    onoff.style.color = "rgb(128, 141, 128)";
+}
+}
+
+function compareColour(col1, col2) {
+    var e = document.createElement('span')
+    document.body.appendChild(e);
+    // standardise
+    e.style.color = col1;
+    col1 = window.getComputedStyle(e).color;
+    e.style.color = col2;
+    col2 = window.getComputedStyle(e).color;
+    // cleanup
+    document.body.removeChild(e);
+    return col1 === col2;
+}
