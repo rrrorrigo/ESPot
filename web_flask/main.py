c755c6e97db11cfd94a4dd0155cd0141cd2044ff
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-19 23:46:01 -0300

    successful login redirect

--- web_flask/main.py
+++ web_flask/main.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/<user_id>/my_plants', strict_slashes=False)
+@app.route('/my_plants/<string:user_id>', strict_slashes=False)
 def home(user_id=""):
     """testing"""
     usr = storage.get(User, user_id)
@@ -28,7 +28,7 @@ def index():
         pwd = request.form['password']
         if md5(pwd.encode()).hexdigest() == storage.getByUsername(User, username).password:
             user_id = storage.getByUsername(User, username).id
-            return redirect(url_for("my_plants", user_id=user_id))
+            return redirect(url_for("home", user_id=user_id))
         else:
             pass
 
