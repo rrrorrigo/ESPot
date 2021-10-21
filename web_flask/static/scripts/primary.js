a67c5499ed53b8b7bd743e36a36000c35612d09d
rrrorrigo <rodrigobriones54@gmail.com>
2021-10-21 14:50:18 -0300

    added Turn_OFF Turn_ON feature

--- web_flask/static/scripts/primary.js
+++ web_flask/static/scripts/primary.js
@@ -2,37 +2,44 @@ const hum = document.querySelector(".hum");
 const irri = document.querySelector(".irri");
 
 function onoff() {
+    
+window.addEventListener('DOMContentLoaded', (e) => {
     let onoff = document.getElementById("onoff");
-    fetch('http://35.243.197.246:5001/api/pots')
+    $('#onoff').click(function(){
+        let stat = {
+            Turned_ON: false
+        };
+        fetch('http://35.243.197.246:5001/api/pots')
             .then(response => response.json())
             .then(data => {
                 Turned_ON = data[0].Turned_ON;
                 if (Turned_ON) {
                     onoff.style.color = "rgb(70, 117, 70)";
                 } else {
-                    url = "http://35.243.197.246:5001/send_data/10fe8791-7ab2-4302-8848-b0a6d280ae48"
-                    xhr.open("PUT", url);
+                    stat = {
+                        Turned_ON: true
+                    }
                     onoff.style.color = "rgb(128, 141, 128)";
                 }
             })
-    };
-window.addEventListener('DOMContentLoaded', (e) => {
+        $.post("http://35.243.197.246:5001/send_data/10fe8791-7ab2-4302-8848-b0a6d280ae48", stat, function(data, status){})
+    })
     function updateAllEvents() { 
-        const tank_text = document.querySelector(".alertext");
-        
-        fetch('http://35.243.197.246:5001/api/pots')
-        .then(response => response.json())
-        .then(data => {
-            hum.innerText = "Humidity: " + data[0].Actual_humidity
-            irri.innerText = "Last irrigation: " + data[0].Last_irrigation
-            if (data[0].Is_empty) {
-                $(".alertt").attr("background", "url('/../static/img/alertt.png') center center");
-                tank_text.innerHTML = "<h5>Warning</h5><br><h6>The water level is low, please add water to the tank!</h6>"
-            } else {
-                $(".alertt").attr("background", "url('/../static/img/tankfull.png') center center");
-                tank_text.innerHTML = "<h5>The tank has enough water</h5>"
-            }
-        })   
+    const tank_text = document.querySelector(".alertext");
+
+    fetch('http://35.243.197.246:5001/api/pots')
+    .then(response => response.json())
+    .then(data => {
+        hum.innerText = "Humidity: " + data[0].Actual_humidity
+        irri.innerText = "Last irrigation: " + data[0].Last_irrigation
+        if (data[0].Is_empty) {
+            $(".alertt").attr("background", "url('/../static/img/alertt.png') center center");
+            tank_text.innerHTML = "<h5>Warning</h5><br><h6>The water level is low, please add water to the tank!</h6>"
+        } else {
+            $(".alertt").attr("background", "url('/../static/img/tankfull.png') center center");
+            tank_text.innerHTML = "<h5>The tank has enough water</h5>"
+        }
+    })   
     }
     let display = setInterval(updateAllEvents, 2000);
 });
