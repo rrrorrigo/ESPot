f30c3998477fce31a4a21611f05df39cab835d09
rrrorrigo <rodrigobriones54@gmail.com>
2021-10-21 20:43:00 -0300

    fixed h6

--- web_flask/static/scripts/primary.js
+++ web_flask/static/scripts/primary.js
@@ -55,10 +55,10 @@ window.addEventListener('DOMContentLoaded', (e) => {
         hum.text = data[0].Actual_humidity
         irri.text = data[0].Last_irrigation
         if (data[0].Is_empty) {
-            $(".alertt").css({"background": "url('/../static/img/alertt.png') top center", "background-repeat": "no-repeat"});
+            $(".alerticon").css({"background": "url('/../static/img/alertt.png') top center", "background-repeat": "no-repeat"});
             tank_text.innerHTML = "<h5>Warning</h5><h6>The water level is low, please add water to the tank!</h6>"
         } else {
-            $(".alertt").css({"background": "url('/../static/img/tankfull.png') top center", "background-repeat": "no-repeat"});
+            $(".alerticon").css({"background": "url('/../static/img/tankfull.png') top center", "background-repeat": "no-repeat"});
             tank_text.innerHTML = "<h5>The tank has enough water</h5>"
         }
     })   
