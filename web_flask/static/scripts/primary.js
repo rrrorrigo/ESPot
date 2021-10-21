a5cac33985b84309323a0ff57a7a6b0ba8713b6d
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-21 13:26:16 -0300

    s

--- web_flask/static/scripts/primary.js
+++ web_flask/static/scripts/primary.js
@@ -24,16 +24,19 @@ window.addEventListener('DOMContentLoaded', (e) => {
     function updateAllEvents() { 
         const hum = document.querySelector(".hum")
         const irri = document.querySelector(".irri")
+        const tank_text = document.querySelector(".alertext")
         
         fetch('http://35.243.197.246:5001/api/pots')
         .then(response => response.json())
         .then(data => {
             hum.innerText = "Humidity: " + data[0].Actual_humidity
-            irri.innerText = "Time of last irrigation: " + data[0].Last_irrigation
+            irri.innerText = "Last irrigation: " + data[0].Last_irrigation
             if (data[0].Is_empty) {
-                $(".tank").attr( "src", "/../static/img/alertt.png" );
+                $(".alertt").attr("background", "url('/../static/img/alertt.png') center center");
+                tank_text.innerHTML = "<h5>Warning</h5><br><h6>The water level is low, please add water to the tank!</h6>"
             } else {
-                $(".tank").attr( "src", "/../static/img/tankfull.png" );
+                $(".alertt").attr("background", "url('/../static/img/tankfull.png') center center");
+                tank_text.innerHTML = "<h5>The tank has enough water</h5>"
             }
         })   
     }
