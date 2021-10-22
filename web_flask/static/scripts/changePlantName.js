2f7d1f371dbb33e13aa72aeda45e42c500a04541
R <rodrigobriones54@gmail.com>
2021-10-22 01:56:33 +0000

    debugging

--- web_flask/static/scripts/changePlantName.js
+++ web_flask/static/scripts/changePlantName.js
@@ -5,8 +5,9 @@ window.addEventListener('DOMContentLoaded', (e) => {
         const plantName = document.getElementsByClassName('plantName')
         const selectedPlant = document.getElementById('selected')
         fetch("http://35.243.197.246:5001/api/selected/10fe8791-7ab2-4302-8848-b0a6d280ae48")
-        .then(response => response.json())
-        .then(data => {
+		.then(response => response.json())
+		.then(data => {
+		    console.log(data);
                 plantName.innerHTML = "<div class='plantName'>" + data.Plant.Plant_name + "</div>"
                 selectedPlant.innerHTML = "<a class='dropdown-item' href='#' id='selected'>" + data.Plant.Plant_name +"</a>"
         });}
