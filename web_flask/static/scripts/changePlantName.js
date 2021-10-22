aaac16ce0f9cd6bb1de4a54611fdd4b845914269
rrrorrigo <rodrigobriones54@gmail.com>
2021-10-22 01:09:38 -0300

    change hardcoded

--- web_flask/static/scripts/changePlantName.js
+++ web_flask/static/scripts/changePlantName.js
@@ -19,7 +19,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
         function selectPlantName(name) {
                 const data = {
-                        "Plant_name": name
+                        "Plant_name": String(name)
                 };
                 $.ajax({
                         url: 'http://35.243.197.246:5001/api/selected/10fe8791-7ab2-4302-8848-b0a6d280ae48 ',
