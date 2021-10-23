bce592466f15b0749ad314e287f03b639fdcb674
rrrorrigo <rodrigobriones54@gmail.com>
2021-10-23 19:04:06 -0300

    updated change button

---
+++ web_flask/static/scripts/changeButtonStatus.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', (e) => {
+    changeTextButton();
+    const onoff = document.getElementById("onofftext");
+
+    $('#onoff').click(function (){
+        changeTextButton();
+        });
+    function changeTextButton() {
+    fetch('http://35.243.197.246:5001/api/pots').then(response => response.json())
+    .then(data => {
+        if (data[0].Turned_ON) {
+            onoff.textContent = "ON";
+            onoff.style.color = "rgb(70, 117, 70)";
+            onoff.style.paddingLeft = "27%";
+        } else {
+            onoff.innerHTML = "OFF";
+            onoff.style.color = "rgb(128, 141, 128)";
+            onoff.style.paddingLeft = "23%";
+        }});
+    };
+
+    function changeTextByButton() {
+        
+    }
+});
