551bec33b9eff45d21dcfa72155019c1ac349084
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-10-23 03:11:08 -0300

    q

--- api/views/api.py
+++ api/views/api.py
@@ -52,7 +52,9 @@ def get_user_pots(user_id):
     pots_list = []
     for pot in storage.all(Pot).values():
         if pot.Username == usr.username:
+            print(storage.get(Plant, pot.Plant_id).Plant_name)
             pot.to_dict()["name"] = storage.get(Plant, pot.Plant_id).Plant_name
+            print(pot.to_dict())
             pots_list.append(pot.to_dict())
     return jsonify(pots_list)
 
